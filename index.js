426cadfd1df58ebb515403cbf7794a1d7bb2bfa6
qAbdullajon <azikmelor7705@gmail.com>
2025-01-05 13:37:42 +0500

    cors

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 const app = express();
 app.use(express.json());
 app.use(
