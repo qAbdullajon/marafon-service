bce12745d731acaf4be03af55d66559141420ee4
qAbdullajon <azikmelor7705@gmail.com>
2025-01-17 12:29:11 +0500

    aasas

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,10 +3,15 @@ const postService = require("../service/post.service");
 class PostController {
   async getAll(req, res, next) {
     try {
-      const data = await postService.getAll();
-      res.status(200).json(data);
+      const workbook = await postService();
+
+      res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+      res.setHeader("Content-Disposition", "attachment; filename=users.xlsx");
+
+      await workbook.xlsx.write(res);
+      res.end();
     } catch (error) {
-      console.log(error);
+      res.status(500).send("Error while generating Excel file.");
     }
   }
   async post(req, res, next) {
--- service/post.service.js
+++ service/post.service.js
@@ -1,9 +1,28 @@
 const postModel = require("../models/post.model");
+const express = require("express");
 
 class PostService {
   async getAll() {
-    const data = await postModel.find();
-    return data;
+    // MongoDB'dan ma'lumotlarni olish
+    const users = await postModel.find();
+
+    // Excel fayl yaratish
+    const workbook = new express.Workbook();
+    const worksheet = workbook.addWorksheet("Users");
+
+    // Sarlavhalar qo'shish
+    worksheet.columns = [
+      { header: "ID", key: "_id", width: 30 },
+      { header: "Name", key: "name", width: 20 },
+      { header: "Email", key: "email", width: 30 },
+    ];
+
+    // Ma'lumotlarni joylash
+    users.forEach((user) => {
+      worksheet.addRow(user);
+    });
+
+    return workbook; // Excel fayl obyektini qaytarish
   }
   async post(post) {
     const data = await postModel.create(post);
